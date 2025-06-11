80444ed31c1c0f12c73849bb75010baa536ad3e2
pedroggwp <phsnnascimento@outlook.com>
2025-06-11 07:48:19 -0300

    some adjusts

--- flask_chat/app/gemini/juiz.py
+++ flask_chat/app/gemini/juiz.py
@@ -11,7 +11,8 @@ api_key = os.getenv("GEMINI_API_KEY")
 juiz = ChatGoogleGenerativeAI(
     model="gemini-2.0-flash",
     temperature=0.3,
-    google_api_key=api_key
+    google_api_key=api_key,
+    convert_system_message_to_human=True
 )
 
 prompt_juiz = '''
--- flask_chat/app/gemini/modelo.py
+++ flask_chat/app/gemini/modelo.py
@@ -10,5 +10,6 @@ api_key = os.getenv("GEMINI_API_KEY")
 model = ChatGoogleGenerativeAI(
     model="gemini-2.0-flash",
     temperature=0.5,
-    google_api_key=api_key
+    google_api_key=api_key,
+    convert_system_message_to_human=True
 )
